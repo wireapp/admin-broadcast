2a6a891869e2f886f42ae799e8b921421c978c8f
Lukas Forst <lukas@forst.pw>
2021-02-12 11:27:37 +0100

    add multiauth

--- app.ts
+++ app.ts
@@ -1,22 +1,23 @@
-import { Application, Router, RouterContext } from 'https://deno.land/x/oak@v6.5.0/mod.ts';
+import { Application, isHttpError, Router, RouterContext } from 'https://deno.land/x/oak@v6.5.0/mod.ts';
 
 const env = Deno.env.toObject();
 
 const romanBroadcast = env.ROMAN_BROADCAST ?? 'https://roman.integrations.zinfra.io/broadcast';
-const romanServiceAuth = env.ROMAN_SERVICE_AUTH;
-const romanAppKey = env.ROMAN_APP_KEY;
-const adminUserId = env.ADMIN_ID;
+const authConfigurationPath = env.AUTH_CONFIGURATION_PATH;
 
 const app = new Application();
 const router = new Router();
 
 router.post('/roman', async (ctx: RouterContext) => {
-  const authorized = ctx.request.headers.get('authorization')?.split(' ')?.find(x => x === romanServiceAuth);
-  ctx.assert(authorized, 401, 'Authorization required.');
+  const [, authorizationToken] = ctx.request.headers.get('authorization')?.split(' ') ?? [];
+  ctx.assert(authorizationToken, 401, 'Authorization required.');
+
+  const { adminId, appKey } = await getConfigurationForAuth(authorizationToken);
+  ctx.assert(appKey, 404, 'No Roman auth found.');
 
   const { type, text, userId } = await ctx.request.body({ type: 'json' }).value;
 
-  if (userId === adminUserId) {
+  if (userId === adminId) {
     if (
       type === 'conversation.init' ||
       (type === 'conversation.new_text' && text.startsWith('/help'))
@@ -26,24 +27,30 @@ router.post('/roman', async (ctx: RouterContext) => {
       type === 'conversation.new_text'
       && text.startsWith('/broadcast')
     ) {
-      await broadcastTextToWire(text.substring(10));
+      await broadcastTextToWire(text.substring(10), appKey);
     }
   } else if (
     type === 'conversation.init'
   ) {
     ctx.response.body = wireMessage('Thanks for subscribing to awesome broadcast.');
   }
+  
   ctx.response.status = 200;
 });
 
+const getConfigurationForAuth = async (authToken: string) => {
+  const authConfiguration = await Deno.readTextFile(authConfigurationPath).then(text => JSON.parse(text));
+  return authConfiguration[authToken];
+};
+
 const wireMessage = (message: string) => ({ type: 'text', text: { data: message } });
 
-const broadcastTextToWire = async (message: string) => {
+const broadcastTextToWire = async (message: string, appKey: string) => {
   const response = await fetch(
     romanBroadcast,
     {
       method: 'POST',
-      headers: { 'app-key': romanAppKey, 'content-type': 'application/json' },
+      headers: { 'app-key': appKey, 'content-type': 'application/json' },
       body: JSON.stringify({ type: 'text', text: { data: message } })
     }
   );
@@ -51,6 +58,35 @@ const broadcastTextToWire = async (message: string) => {
   return response.status;
 };
 
+/* ----------------- WIRE Common ----------------- */
+// k8s indication the service is running
+router.get('/status', ({ response }) => {
+  response.status = 200;
+});
+// technical endpoint to display the version
+router.get('/version', async ({ response }) => {
+  let version: string | undefined;
+  const releaseFilePath = Deno.env.get('RELEASE_FILE_PATH');
+  if (releaseFilePath) {
+    try {
+      version = await Deno.readTextFile(releaseFilePath).then(text => text.trim());
+    } catch {
+    }
+  }
+  response.body = { version: version ?? 'development' };
+});
+// log all failures that were not handled
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    if (!isHttpError(err)) {
+      console.log(err);
+    }
+    throw err;
+  }
+});
+/* //--------------- WIRE Common ----------------- */
 
 app.use(router.routes());
 app.use(router.allowedMethods());
